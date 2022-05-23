60c3622171b4df1271f9c6d2385872725788039a
FlameFate <egor.androsoff@gmail.com>
2022-05-24 00:05:12 +0300

    adding a home screen with a selection of options

--- app/src/main/java/com/example/backgroundapp/MainActivity.kt
+++ app/src/main/java/com/example/backgroundapp/MainActivity.kt
@@ -1,74 +1,34 @@
 package com.example.backgroundapp
 
-import CustomAdapter
+
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
+import android.view.View
+
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.PackageManagerCompat.LOG_TAG
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+
 import com.example.backgroundapp.databinding.ActivityMainBinding
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+
 
 
 class MainActivity : AppCompatActivity() {
-    var cnt = 0
+
     lateinit var bindingClass: ActivityMainBinding
 
     override fun onCreate(s: Bundle?) {
         super.onCreate(s)
         bindingClass = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bindingClass.root)
+var bt = bindingClass.btPresent
+        bt.setOnClickListener {
 
-        val recyclerview = findViewById<RecyclerView>(R.id.recyclerview)
-
-        // this creates a vertical layout Manager
-        recyclerview.layoutManager = GridLayoutManager(this,2)
-
-        // ArrayList of class ItemsViewModel
-        val data = ArrayList<BGItem>()
-
-        // This loop will create 20 Views containing
-        // the image with the count of view
-        for (i in 1..20) {
-            data.add(BGItem(R.drawable.ic_baseline_backpack_24, "Item " + i))
+            val intent = Intent(this@MainActivity, PresentActivity::class.java)
+            startActivity(intent)
         }
 
-        // This will pass the ArrayList to our Adapter
 
 
-        val apiInterface = ApiInterface.create().getMovies()
-
-        //apiInterface.enqueue( Callback<List<Movie>>())
-        apiInterface.enqueue( object : Callback<WallpaperApi> {
-            override fun onResponse(call: Call<WallpaperApi>?, response: Response<WallpaperApi>?) {
-                val adapter = CustomAdapter(response?.body()?.hits)
-
-                // Setting the Adapter with the recyclerview
-                recyclerview.adapter = adapter
-               // if(response?.body() != null)
-                   // recyclerAdapter.setMovieListItems(response.body()!!)
-            }
-
-            override fun onFailure(call: Call<WallpaperApi>?, t: Throwable?) {
-
-            }
-        })
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-
-
-    }
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-
-
-    }
+}
 }
 
 
---
+++ app/src/main/java/com/example/backgroundapp/PresentActivity.kt
@@ -0,0 +1,71 @@
+package com.example.backgroundapp
+
+import CustomAdapter
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.backgroundapp.databinding.ActivityMainBinding
+import com.example.backgroundapp.databinding.ActivityPresentBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class PresentActivity : AppCompatActivity() {
+
+    lateinit var bindingClass: ActivityPresentBinding
+
+    override fun onCreate(s: Bundle?) {
+        super.onCreate(s)
+        bindingClass = ActivityPresentBinding.inflate(layoutInflater)
+        setContentView(bindingClass.root)
+
+        val recyclerview = findViewById<RecyclerView>(R.id.recyclerview)
+
+        // this creates a vertical layout Manager
+        recyclerview.layoutManager = GridLayoutManager(this,2)
+
+        // ArrayList of class ItemsViewModel
+        val data = ArrayList<BGItem>()
+
+        // This loop will create 20 Views containing
+        // the image with the count of view
+        for (i in 1..20) {
+            data.add(BGItem(R.drawable.ic_baseline_backpack_24, "Item " + i))
+        }
+
+        // This will pass the ArrayList to our Adapter
+
+
+        val apiInterface = ApiInterface.create().getMovies()
+
+        //apiInterface.enqueue( Callback<List<Movie>>())
+        apiInterface.enqueue( object : Callback<WallpaperApi> {
+            override fun onResponse(call: Call<WallpaperApi>?, response: Response<WallpaperApi>?) {
+                val adapter = CustomAdapter(response?.body()?.hits)
+
+                // Setting the Adapter with the recyclerview
+                recyclerview.adapter = adapter
+                // if(response?.body() != null)
+                // recyclerAdapter.setMovieListItems(response.body()!!)
+            }
+
+            override fun onFailure(call: Call<WallpaperApi>?, t: Throwable?) {
+
+            }
+
+        })
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+
+    }
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+
+    }
+
+}
